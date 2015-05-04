ad931070e2f7d12e452f20a4893f4b094c5388a0
Johahn <aznknight999@gmail.com>
2015-05-03 23:02:07 -0500

    first

---
+++ htp/EvaluateHelp.java
@@ -0,0 +1,38 @@
+package htp;
+
+import java.util.ArrayList;
+
+public class EvaluateHelp {
+	
+	public static boolean getSinglePrecisionApproach2 (ArrayList<String> predictedHashTags, ArrayList<String> actualHashTags){
+		for(String predicted : predictedHashTags){
+			if(actualHashTags.contains(predicted)){
+				return true;
+			}
+		}
+		return false; 
+	}
+	
+	/**
+	 * Can probably be used for precision too
+	 * @param predictedHashTags
+	 * @param actualHashTags
+	 * @return
+	 */
+	public static int getSingleRecallApproach2 (ArrayList<String> predictedHashTags, ArrayList<String> actualHashTags){
+		int count = 0; 
+		for(String predicted : predictedHashTags){
+			if(actualHashTags.contains(predicted)){
+				count++;
+			}
+		}
+		return count; 
+	}
+	
+	public static double precision (int correct, int incorrect){
+		double precision = correct * 1.0 / (correct + incorrect); 
+		return precision; 
+	}
+	
+	 
+}
