74bec1077ca1ff5765dc7bffef426ea47a0fa78b
Matthew Lau <Matthew@wireless-10-145-155-228.public.utexas.edu>
2015-05-06 01:38:56 -0500

    Commented out trainer printing model

--- TwitterHashTagPredictor/src/predictor/POSHashTagPredictor.java
+++ TwitterHashTagPredictor/src/predictor/POSHashTagPredictor.java
@@ -50,9 +50,9 @@ public class POSHashTagPredictor implements HashTagPredictor {
 	@Override
 	public boolean trainModel(List<TweetHashTagTuple> trainingList) {
 		predictionModel = modelTrainer.trainModel(trainingList);
-		for (String s : predictionModel.getModel().keySet()) {
-			System.out.println(s + ":" + predictionModel.getModel().get(s));
-		}
+		// for (String s : predictionModel.getModel().keySet()) {
+		// System.out.println(s + ":" + predictionModel.getModel().get(s));
+		// }
 		return true;
 	}
 
