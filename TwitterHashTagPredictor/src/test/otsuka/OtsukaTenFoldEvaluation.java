0272b7ac48c9d91ce423d090bc42bdb08409f8b6
Matthew Lau <Matthew@Matthews-MacBook-Pro.local>
2015-05-12 02:30:28 -0500

    added tenfold for otsuka

---
+++ TwitterHashTagPredictor/src/test/otsuka/OtsukaTenFoldEvaluation.java
@@ -0,0 +1,36 @@
+package test.otsuka;
+
+import htfidh.HTFIDHHashTagPredictor;
+import io.TweetHashTagTuple;
+import io.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import predictor.HashTagPredictor;
+import test.Options;
+import test.TenFoldEvaluation;
+
+public class OtsukaTenFoldEvaluation {
+	public static void main(String[] args) throws IOException {
+		Options options = new Options(args);
+		System.out.println("Loading file " + options.inputFile);
+		List<TweetHashTagTuple> totalList = Utils
+				.loadFilesIntoHashTagTupleList(new File(options.inputFile));
+
+		System.out.println("File to write to " + options.outputFile);
+		try (PrintWriter pw = new PrintWriter(new File(options.outputFile))) {
+			TenFoldEvaluation evaluator = new TenFoldEvaluation();
+			HashTagPredictor predictor = new HTFIDHHashTagPredictor();
+			List<Integer> numPredictions = new ArrayList<Integer>();
+			for (int i = 0; i < 20; i++)
+				numPredictions.add(i);
+			evaluator.tenFoldEvaluateAndWrite(predictor, totalList,
+					numPredictions, pw, options.evalOptions);
+		}
+
+	}
+}
